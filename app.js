e6c8a4098be8c56a3abda1c6833cc885a69cfa68
jukkahyvarin <jukka.hyvarin@gmail.com>
2013-09-13 13:00:16 +0300

    configure

--- app.js
+++ app.js
@@ -1,23 +1,74 @@
 var express = require('express');
+var Client = require('node-rest-client').Client;
 
 var app = express();
+var fs = require('fs');
+xml2js = require('xml2js');
+
+
 
 app.get('/', function(req, res) {
-    //res.redirect('/dev.html');
-    res.redirect('app/index.html');
+   
+    res.redirect('index.html');
 });
+
+// rest method for getting areas
+app.get('/movies/getareas', function (req, res) {
+      
+    client = new Client();
+    client.get('http://www.finnkino.fi/xml/TheatreAreas/', function(data, response){
+        var parser = new xml2js.Parser({explicitArray:false});
+        parser.parseString(data, function(err, result) {
+            res.json(result);
+        });
+    });
+    
+});
+
+// rest method for getting schedule dates
+app.get('/movies/getscheduledates/:area', function (req, res) {
+ 
+    client = new Client();
+    client.get('http://www.finnkino.fi/xml/ScheduleDates/?area=' + req.params.area, function(data, response){
+        var parser = new xml2js.Parser({explicitArray:false});
+        parser.parseString(data, function(err, result) {
+            res.json(result);
+        });
+    });
+});
+
+// rest method for getting movies
+app.get('/movies/getschedule/:area/:date', function (req, res) {
+ 
+    client = new Client();
+    client.get('http://www.finnkino.fi/xml/Schedule/?area=' + req.params.area + '&dt=' + req.params.date, function(data, response){
+        var parser = new xml2js.Parser({explicitArray:false});
+        parser.parseString(data, function(err, result) {
+            res.json(result);
+        });
+    });
+});
+
+// rest method for getting movie information
+app.get('/movies/getschedule/:area/:date/:id', function (req, res) {
+ 
+    client = new Client();
+    client.get('http://www.finnkino.fi/xml/Schedule/?area=' + req.params.area + '&dt=' + req.params.date + '&eventID=' + req.params.id, function(data, response){
+        var parser = new xml2js.Parser({explicitArray:false});
+        parser.parseString(data, function(err, result) {
+            res.json(result);
+        });
+    });
+});
+
 
 app.configure(function() {
     app.use(express.methodOverride());
     app.use(express.bodyParser());
 
-    // TODO:
-    //  * Add configuration loading
-    //app.use(express.static('../exakti-client-web/app'));
-
-    // app.use(express.static('../exakti-client-web/release'));
-    // app.use(express.static('../exakti-client-web/release/img'));
-
+  
+    app.use(express.static(__dirname + '/app'));
+   
     app.use(express.errorHandler({
         dumpExceptions: true,
         showStack: true
--- app/js/services/moviesService.js
+++ app/js/services/moviesService.js
@@ -7,22 +7,22 @@ app.service('moviesService', function ($http) {
     return {
         getAreas: function () {
             //return $http.get('http://localhost:49871/api/movies/getareas').then(function (data, status, headers, config) {
-            return $http.get('http://mynodeservice.azurewebsites.net/movies/getareas').then(function (data, status, headers, config) {
+            return $http.get('/movies/getareas').then(function (data, status, headers, config) {
                 return data.data;
             });
         },
         getSchedule: function (area, date) {
-            return $http.get('http://mynodeservice.azurewebsites.net/movies/getschedule/' + area + '/' + date).then(function (data, status, headers, config) {
+            return $http.get('/movies/getschedule/' + area + '/' + date).then(function (data, status, headers, config) {
                 return data.data;
             });
         },
         getScheduleDates: function (area) {
-            return $http.get('http://mynodeservice.azurewebsites.net/movies/getscheduledates/' + area).then(function (data, status, headers, config) {
+            return $http.get('/movies/getscheduledates/' + area).then(function (data, status, headers, config) {
                 return data.data;
             });
         },
         getMovie: function (area, id, date) {
-            return $http.get('http://mynodeservice.azurewebsites.net/movies/getschedule/' + area + '/' + date + '/' + id).then(function (data, status, headers, config) {
+            return $http.get('/movies/getschedule/' + area + '/' + date + '/' + id).then(function (data, status, headers, config) {
                 return data.data;
             });
         }
